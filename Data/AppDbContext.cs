1b770e870a2c34e001751e9f605f286dfe0bf208
Nayelydaza20 <nayelydaza3@gmail.com>
2021-11-11 09:41:43 -0400

    AppDbContext creado

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFaza.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFaza.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<Muebles>Muebles { get; set; } 
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+
+        {
+        }
+    }
+}
