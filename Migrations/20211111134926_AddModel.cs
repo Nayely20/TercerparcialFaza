877ef47093405f3f197cba01cecb115c5ac38038
Nayelydaza20 <nayelydaza3@gmail.com>
2021-11-11 09:50:19 -0400

    Migrations Implementado

---
+++ Migrations/20211111134926_AddModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoFaza.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Muebles",
+                columns: table => new
+                {
+                    Mueble = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Marca = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Precio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Enlace = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Muebles", x => x.Mueble);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Muebles");
+        }
+    }
+}
